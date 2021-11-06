126d7829781975792bb7c191dab36914fae5db58
Anastasiia <nastyasvstpl@gmail.com>
2021-11-06 11:49:56 +0200

    innit commit

---
+++ app/src/main/java/com/app/anastasia/application/country/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.anastasia.application.country
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
